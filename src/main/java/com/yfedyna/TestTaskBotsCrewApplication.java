8b58d018e475751a763ae7ba2b6e9fd954966346
yfedyna <yfedyna@griddynamics.com>
2023-05-08 12:00:07 +0300

    init

---
+++ src/main/java/com/yfedyna/TestTaskBotsCrewApplication.java
@@ -0,0 +1,13 @@
+package com.yfedyna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskBotsCrewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskBotsCrewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yfedyna/TestTaskBotsCrewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yfedyna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskBotsCrewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
